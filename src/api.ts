59d92303b218f0e5a07772febc63b9a7140d1ef6
Finn Llewellyn <llewellynfinn@gmail.com>
2025-07-22 20:53:08 +0100

    Added from query param to races endpoint

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,8 @@ import { TRace } from './types/race.type';
 
 // Revalidates every 4 hours
 export async function fetchRaces(): Promise<TRace[]> {
-  return get<TRace[]>('/races', {
+  const fromDatetime = new Date().toISOString();
+  return get<TRace[]>(`/races?from=${fromDatetime}`, {
     tags: ['races'],
     revalidate: 4 * 1000 * 60 * 60,
   });
